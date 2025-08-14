43830a5f70f5e76d85d4d8f16aa6ead43eea35b6
aryan7071 <aryan002005@gmail.com>
2025-08-15 01:56:01 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const {
     register,
     handleSubmit,
-    
+    setError,
     formState: { errors , isSubmitting },
   } = useForm()
 
@@ -20,9 +20,13 @@ function App() {
   }
 
   const onSubmit = async (data) => {
-    await delay(2)
-    console.log(data)
+    //await delay(2)
+    let r = await  fetch("http://localhost:3000/", {method: "POST" , body:JSON.stringify(data)})
+    let res = await r.text()
+    console.log(data , res)
   }
+
+
   return (
     <>
     {isSubmitting && <div>Loading</div>}
@@ -32,9 +36,12 @@ function App() {
           {errors.username && <div className="red" > {errors.username.message } </div>}
 
           <br />
-          <input placeholder="Password" {...register("password")} type="password" />
+          <input placeholder="Password" {...register("password" , {minLength: { value: 7 , message: "Minimum password of 7 length required" } })} type="password" />
+           {errors.password && <div className="red" > {errors.password.message } </div>}
+
           <br />
-          <input disabled={isSubmitting} type="submit" name="submit" />
+          <input  disabled={isSubmitting} type="submit" name="submit" />
+          {errors.Myform && <div className="red" > {errors.Myform.message } </div>}
         </form>
       </div>
     </>
